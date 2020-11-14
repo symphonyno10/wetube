cb86fa5dea36782076ddb04b71f915c8c4c91965
WJ Cho <symphonyno10@gmail.com>
2020-11-15 02:32:02 +0900

    route

--- index.js
+++ index.js
@@ -2,8 +2,21 @@ const express = require("express");
 const app = express();
 
 const PORT = 4000;
+
 function handleListening() {
   console.log(`Listening on : http://localhost:${PORT}`);
 }
 
+function handleHome(req, res) {
+  console.log(req);
+  res.send("homehome");
+}
+
+function handleProfile(req, res) {
+  res.send("profile");
+}
+
 app.listen(PORT, handleListening);
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
