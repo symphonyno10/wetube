73ae1a162a2d80002d6fd1def5716fe563562a64
WJ Cho <symphonyno10@gmail.com>
2020-11-15 02:52:06 +0900

    babel nodemon

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-const express = require("express");
+//const express = require("express");
+
+import express from "express";
 const app = express();
 
 const PORT = 4000;
 
-function handleListening() {
+const handleListening = () =>
   console.log(`Listening on : http://localhost:${PORT}`);
-}
 
-function handleHome(req, res) {
+const handleHome = (req, res) => {
   console.log(req);
   res.send("homehome");
-}
+};
 
-function handleProfile(req, res) {
-  res.send("profile");
-}
+const handleProfile = (req, res) => res.send("profile");
 
 app.listen(PORT, handleListening);
 
