8f42aa9dbe2171c8cbd0a207fc6db0a9923c40da
WJ Cho <symphonyno10@gmail.com>
2020-11-15 03:06:55 +0900

    middle ware

--- index.js
+++ index.js
@@ -8,14 +8,18 @@ const PORT = 4000;
 const handleListening = () =>
   console.log(`Listening on : http://localhost:${PORT}`);
 
-const handleHome = (req, res) => {
-  console.log(req);
-  res.send("homehome");
-};
+const handleHome = (req, res) => res.send("homehome");
 
 const handleProfile = (req, res) => res.send("profile");
 
+const betweenHome = (req, res, next) => {
+  console.log("between");
+  next();
+};
+
 app.listen(PORT, handleListening);
+
+app.use(betweenHome);
 
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
