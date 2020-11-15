986c86118c4d536769388c1fd50613a50c944a04
WJ Cho <symphonyno10@gmail.com>
2020-11-15 09:53:35 +0900

    middleware

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 //const express = require("express");
 
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express();
 
 const PORT = 4000;
@@ -12,14 +17,13 @@ const handleHome = (req, res) => res.send("homehome");
 
 const handleProfile = (req, res) => res.send("profile");
 
-const betweenHome = (req, res, next) => {
-  console.log("between");
-  next();
-};
-
-app.listen(PORT, handleListening);
-
-app.use(betweenHome);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
