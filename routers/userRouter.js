d685726a405887f2360eb31dea6222104b75a379
WJ Cho <symphonyno10@gmail.com>
2020-11-15 22:57:43 +0900

    controller finish

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,9 +1,17 @@
 import express from "express";
+import {
+  chagePassword,
+  editProfile,
+  userDetail,
+  users,
+} from "../controllers/usercontroller";
 import routes from "../routes";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.users, (req, res) => res.send("Users"));
-userRouter.get(routes.userDetail, (req, res) => res.send("User Detail"));
+userRouter.get(routes.users, users);
+userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.changePassword, chagePassword);
 
 export default userRouter;
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,6 +1,19 @@
 import express from "express";
+import routes from "../routes";
+import {
+  deleteVideo,
+  editVideo,
+  upload,
+  videoDetail,
+  videos,
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
+videoRouter.get(routes.videos, videos);
+videoRouter.get(routes.upload, upload);
+videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.editVideo, editVideo);
+videoRouter.get(routes.deleteVideo, deleteVideo);
 
 export default videoRouter;
