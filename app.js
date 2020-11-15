b42f5a6f2f13593602a22746aebda07625f157f2
WJ Cho <symphonyno10@gmail.com>
2020-11-15 21:35:06 +0900

    controller

--- app.js
+++ app.js
@@ -5,13 +5,12 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 
 const app = express();
-
-const handleHome = (req, res) => res.send("homehome");
-
-const handleProfile = (req, res) => res.send("profile");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,8 +18,8 @@ app.use(cookieParser());
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", handleHome);
-app.get("/profile", handleProfile);
-app.use("/user", userRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
---
+++ routers/userRouter.js
@@ -0,0 +1,9 @@
+import express from "express";
+import routes from "../routes";
+
+const userRouter = express.Router();
+
+userRouter.get(routes.users, (req, res) => res.send("Users"));
+userRouter.get(routes.userDetail, (req, res) => res.send("User Detail"));
+
+export default userRouter;
